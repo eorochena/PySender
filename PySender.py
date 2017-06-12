993a165afed3da2b33be2920e5650df9662717e6
Eduardo Orochena <mint.alchemist@gmail.com>
2017-06-11 20:05:33 -0400

    threading

--- PySender.py
+++ PySender.py
@@ -30,12 +30,13 @@ def log_file(app):
     logging_file = pysender_logdir + '/' + app
     return logging_file
 
-def tail_it(filename):
+def tail_it():
     tail_it = subprocess.Popen(['tail', '-F', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return tail_it
 
+tail_pid = tail_it().pid
+
 def tail_alive(filename, app):
-    tail_pid = tail_it(filename).pid
     alive = os.popen('ps ax|grep "tail -F %s"|grep -w "%s"|grep -v grep|wc -l' % (filename, tail_pid)).read()[0]
     if int(alive) >= 1:
         return True
@@ -65,7 +66,7 @@ def firestart_status(app):
 def logmsg(app):
     content = ''
     while True:
-        msg = tail_it(filename).stdout.readline()
+        msg = tail_it().stdout.readline()
         try:
             if len(msg) > 0 and len(content) <= 109186:
                 #if re.search(r'^[0-9]', msg) and re.search(r'(\d+-\d+-\d+)', msg):
@@ -102,7 +103,7 @@ def run_while(filename, app):
                 log_it.write(today_date + ' - unable to send message to GrayLog - ' + str(e) + '\n')
                 log_it.close()
         elif not tail_alive(filename, app) and firestart_status(app):
-            tail_it(filename)
+            tail_it()
             today_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
             log_it = open(log_file(app), 'a+')
             log_it.write(today_date + ' - restarting tail_it\n')
