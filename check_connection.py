af86a0455e35fad26a8bd6443460a9628ee0f5c5
Eduardo Orochena <mint.alchemist@gmail.com>
2017-12-26 20:27:14 -0500

    preventing leaks of file descriptors by using with

--- check_connection.py
+++ check_connection.py
@@ -3,7 +3,6 @@
 import socket
 import datetime
 import os
-import sys
 import subprocess
 import sender
 
@@ -11,7 +10,6 @@ graylog_server = sender.graylog_server()
 graylog_monitor_port = int(sender.monitor_port())
 port = int(sender.graylog_input_port())
 
-# Application log
 LogDir = "/var/log/pysender/"
 LogFile = "/var/log/pysender/check_connection.log"
 today_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
@@ -29,12 +27,10 @@ def status():
         check_it.send('connected to GrayLog'.encode('utf-8'))
         check_it.shutdown(1)
         check_it.close()
-        log_file = open(LogFile, 'a+')
-        log_file.write(hoy + ' Connected to Graylog input on port %s\n' % port)
-        log_file.close()
+        with open(LogFile, 'a+') as log_file:
+            log_file.write(hoy + ' Connected to Graylog input on port %s\n' % port)
         return True
     except Exception as error:
-        log_file = open(LogFile, 'a+')
-        log_file.write(hoy + ' - socket - Unable to establish connection with GrayLog - ' + str(error) + '\n')
-        log_file.close()
+        with open(LogFile, 'a+') as log_file:
+            log_file.write(hoy + ' - socket - Unable to establish connection with GrayLog - ' + str(error) + '\n')
         return False
