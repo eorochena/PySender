d6085a0f8166f48b8386da7067f395bee9065965
Eduardo Orochena <mint.alchemist@gmail.com>
2017-12-26 20:33:12 -0500

    Revert "preventing leaks of file descriptors by using with"
    This reverts commit af86a0455e35fad26a8bd6443460a9628ee0f5c5.

--- check_connection.py
+++ check_connection.py
@@ -3,6 +3,7 @@
 import socket
 import datetime
 import os
+import sys
 import subprocess
 import sender
 
@@ -10,6 +11,7 @@ graylog_server = sender.graylog_server()
 graylog_monitor_port = int(sender.monitor_port())
 port = int(sender.graylog_input_port())
 
+# Application log
 LogDir = "/var/log/pysender/"
 LogFile = "/var/log/pysender/check_connection.log"
 today_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
@@ -27,10 +29,12 @@ def status():
         check_it.send('connected to GrayLog'.encode('utf-8'))
         check_it.shutdown(1)
         check_it.close()
-        with open(LogFile, 'a+') as log_file:
-            log_file.write(hoy + ' Connected to Graylog input on port %s\n' % port)
+        log_file = open(LogFile, 'a+')
+        log_file.write(hoy + ' Connected to Graylog input on port %s\n' % port)
+        log_file.close()
         return True
     except Exception as error:
-        with open(LogFile, 'a+') as log_file:
-            log_file.write(hoy + ' - socket - Unable to establish connection with GrayLog - ' + str(error) + '\n')
+        log_file = open(LogFile, 'a+')
+        log_file.write(hoy + ' - socket - Unable to establish connection with GrayLog - ' + str(error) + '\n')
+        log_file.close()
         return False
