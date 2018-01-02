0031c0c6b3fec566af38bd875c2bdf7c413caa57
Eduardo Orochena <mint.alchemist@gmail.com>
2018-01-02 13:29:55 -0500

    unifying log entries

--- graylog_status.py
+++ graylog_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import cloghandler
+import datetime
 import requests
 import sys
 import json
 import sender
+import logging.config
 import logging
 import subprocess
 import os
@@ -15,70 +18,66 @@ admin_password = sender.graylog_admin_password()
 servers = []
 results = []
 journal_capacity = []
+facility = 'graylog-api'
 
 if os.path.isdir('/var/log/pysender') == False:
     subprocess.call('mkdir /var/log/pysender', shell=True)
 
-logging_file = "/var/log/pysender/graylog_status.log"
-logging.basicConfig(format='%(asctime)s %(message)s', filename=logging_file, level=logging.INFO)
+logging.config.fileConfig('logging.conf')
+log = logging.getLogger()
 
 def graylog_state():
     try:
-            response = requests.get('http://%s:%s/system/lbstatus' % (graylog_server, load_balancer_port),
+        response = requests.get('http://%s:%s/system/lbstatus' % (graylog_server, load_balancer_port),
                                 timeout = 18, headers={'Connection':'close'})
-            if response.text == 'ALIVE':
-                    try:
-                            response = requests.get('http://%s:%s/cluster' % (graylog_server, load_balancer_port),
-                                                timeout = 18, headers={'Connection':'close'},
-                                                auth = (admin_user, admin_password))
-                            val = json.loads(response.text)
-                            for i in val:
-                                servers.append(i)
-                            for i in range(len(servers)):
-                                if val[servers[i]]['is_processing'] == True:
-                                        results.append('True')
-                                        continue
-                                elif val[servers[i]]['is_processing'] == False:
-                                        results.append('False')
-                                        continue
-                            if 'False' in results:
-                                logging.critical('CRITICAL - GrayLog is not processing incoming messages')
-                                return False
-                                sys.exit(1)
-                            else:
-                                for i in range(len(servers)):
-                                    try:
-                                        response = requests.get('http://%s:%s/cluster/%s/journal'
-                                                % (graylog_server, load_balancer_port, servers[i]), timeout = 18,
-                                                headers = {'Connection':'close'}, auth = (admin_user, admin_password))
-                                        journal_status = json.loads(response.text)
-                                        journal_usage = (float(journal_status['journal_size'])/
-                                                         float(journal_status['journal_size_limit']))*100
-                                        if journal_usage >= 75:
-                                            journal_capacity.append(journal_usage)
-                                        elif journal_usage <= 74:
-                                            continue
-                                    except Exception as error:
-                                        logging.critical('CRITICAL - unable to get journal status - %s' % error)
-                                        return False
-                                        sys.exit(1)
-                                if journal_capacity:
-                                        logging.critical('CRITICAL - journal capacity = ' + str(journal_capacity))
-                                        return False
-                                        sys.exit(1)
-                                else:
-                                        return True
-                                        sys.exit(0)
-                    except Exception as error:
-                        logging.critical('CRITICAL - unable to get status response from GrayLog - %s' % error)
-                        return False
-                        sys.exit(1)
-            else:
-                logging.critical('CRITICAL - Bad response from GrayLog - ', response.content, ' ', response.status_code)
+        if response.text == 'ALIVE':
+            try:
+                response = requests.get('http://%s:%s/cluster' % (graylog_server, load_balancer_port),
+                                            timeout = 18, headers={'Connection':'close'},
+                                            auth = (admin_user, admin_password))
+                val = json.loads(response.text)
+                for i in val:
+                    servers.append(i)
+                for i in range(len(servers)):
+                    if val[servers[i]]['is_processing'] == True:
+                        results.append('True')
+                        continue
+                    elif val[servers[i]]['is_processing'] == False:
+                        results.append('False')
+                        continue
+                if 'False' in results:
+                    log.critical('%s GrayLog is not processing incoming messages' % facility)
+                    return False
+                else:
+                    for i in range(len(servers)):
+                        try:
+                            response = requests.get('http://%s:%s/cluster/%s/journal'
+                                                    % (graylog_server, load_balancer_port, servers[i]),timeout = 18,
+                                                    headers = {'Connection':'close'},
+                                                    auth = (admin_user, admin_password))
+                            journal_status = json.loads(response.text)
+                            journal_usage = (float(journal_status['journal_size'])/
+                                             float(journal_status['journal_size_limit']))*100
+                            if journal_usage >= 75:
+                                journal_capacity.append(journal_usage)
+                            elif journal_usage <= 74:
+                                continue
+                        except Exception as error:
+                            log.critical('%s CRITICAL - unable to get journal status - %s' % (facility, error))
+                            return False
+                    if journal_capacity:
+                            log.critical('%s CRITICAL - journal capacity = %s' % (facility, str(journal_capacity)))
+                            return False
+                    else:
+                        return True
+            except Exception as error:
+                log.critical('%s CRITICAL - unable to get status response from GrayLog - %s' % (facility, error))
                 return False
-                sys.exit(1)
-    except Exception as error:
-            logging.critical('CRITICAL - unable to get any response from GrayLog - %s ' % error)
+        else:
+            log.critical('%s CRITICAL - Bad response from GrayLog - %s %s', (facility, response.content,
+                                                                             response.status_code))
             return False
-            sys.exit(1)
+    except Exception as error:
+        log.critical('%s CRITICAL - unable to get any response from GrayLog - %s ' % (facility, error))
+        return False
 
--- pysender.py
+++ pysender.py
@@ -10,120 +10,82 @@ import sys
 import time
 import check_connection
 import graylog_status
+import logging.config
+import logging
+import cloghandler
 
 graylog_server = sender.graylog_server()
 graylog_monitor_port = int(sender.monitor_port())
 graylog_port = int(sender.graylog_input_port())
 ip_address = socket.gethostbyname(socket.gethostname())
-today_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
-pysender_logdir = '/var/log/pysender'
-
+logging.config.fileConfig('logging.conf')
+log = logging.getLogger()
 
 def pysender(filename, app, hostname):
+    facility = 'pysender-%s' % app
     tail_it = subprocess.Popen(['tail', '-F', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-
-    def log_file(app):
-        if not os.path.isdir(pysender_logdir):
-            os.mkdir('/var/log/pysender')
-        logging_file = pysender_logdir + '/pysender-%s.log' % app
-        return logging_file
-
 
     def tail_alive(filename, app):
         try:
             if tail_it.poll() == None:
                 return True
             else:
-                today_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
-                with open(log_file(app), 'a+') as log_it:
-                    log_it.write(today_date + ' - tail process appears not to be runnning properly - %s - %s\n' %
-                             (filename, app))
+                log.warning('%s tail process appears not to be runnning properly - %s - %s\n' % (facility,
+                                                                                                 filename, app))
                 return False
         except Exception as error:
-            today_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
-            with open(log_file(app), 'a+') as log_it:
-                log_it.write(today_date + ' - tail process appears not to be runnning properly - %s - %s - %s\n' %
-                             (filename, app, error))
+            log.warning('%s tail process appears not to be runnning properly - %s - %s - %s\n' % (facility, filename,
+                                                                                                  app, error))
             return False
 
-
-    def firestart_status(app, firestart_pid):
-        try:
-            os.kill(firestart_pid, 0)
-        except OSError:
-            today_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
-            with open(log_file(app), 'a+') as log_it:
-                log_it.write(today_date + ' - firestart process is not running \n')
-            return False
-        else:
-            return True
-
-
-    def logmsg(app):
+    def logmsg():
         content = ''
         while True:
-            today_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
             msg = tail_it.stdout.readline()
             try:
                 if len(msg) > 0 and len(msg) <= 109186:
                     yield content
                     content = msg
                 elif len(msg) > 109186:
-                    with open(log_file(app), 'a+') as log_it:
-                        log_it.write('%s - Too many characters in message string\n' % today_date)
+                    log.warning('%s Too many characters in message string' % facility)
                     break
                 else:
                     break
-            except Exception as e:
-                with open(log_file(app), 'a+') as log_it:
-                    log_it.write(today_date + ' - failed to process log message - ' + str(e) + '\n')
+            except Exception as error:
+                log.warning('%s failed to process log message - %s' % (facility, str(error)))
                 continue
         if len(content) > 0:
             yield content
 
-    today_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
-    with open(log_file(app), 'a+') as log_it:
-        log_it.write(today_date + ' - Starting application or at least trying - \n')
+    log.info('%s Starting application or at least trying...' % facility)
 
 
     while True:
         if tail_alive(filename, app) and check_connection.status() and graylog_status.graylog_state():
-            today_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
-            with open(log_file(app), 'a+') as log_it:
-                log_it.write(today_date + ' - Sending messages to Graylog - \n')
+            log.info('%s Sending messages to Graylog :)' % facility)
             sender = socket.socket()
             sender.connect_ex((graylog_server, graylog_port))
             try:
-                for line in logmsg(app):
+                for line in logmsg():
                     message = hostname + ' - ' + app + ': ' + str(json.dumps('%s' % line)) + '\n'
                     sender.sendall((message).encode('utf-8'))
             except Exception as error:
-                today_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
-                with open(log_file(app), 'a+') as log_it:
-                    log_it.write(today_date + ' - Unable to send message to Graylog - ' + str(error) + '\n')
+                log.warning('%s Unable to send message to Graylog - %s' % (facility, str(error)))
                 sender.close()
                 continue
         elif tail_alive(filename, app) == False and check_connection.status() and graylog_status.graylog_state():
             tail_it = subprocess.Popen(['tail', '-F', filename], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-            today_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
-            with open(log_file(app), 'a+') as log_it:
-                log_it.write(today_date + ' - restarting tail_it\n')
+            log.warning('%s restarting tail process' % facility)
             continue
         elif tail_alive(filename, app) and check_connection.status() == False and graylog_status.graylog_state():
-            today_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
-            with open(log_file(app), 'a+') as log_it:
-                log_it.write(today_date + ' - Unable to send log messages to Graylog, Graylog Input is not processing '
+            log.warning('%s Unable to send log messages to Graylog, Graylog Input is not processing '
                                       'incoming messages check /var/log/pysender/check_connection.log, '
-                                      'retrying every 10 seconds\n')
+                                      'retrying every 10 seconds' % facility)
             sender.close()
             time.sleep(10)
             continue
         elif tail_alive(filename, app) and check_connection.status() and graylog_status.graylog_state() == False:
-            today_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
-            with open(log_file(app), 'a+') as log_it:
-                log_it.write(today_date + ' - Not sending messages to Graylog, incoming messages check '
-                                      '/var/log/pysender/graylog_status.log, retrying every 10 seconds\n')
+            log.warning('%s Not sending messages to Graylog' % facility)
             time.sleep(10)
             continue
         elif tail_alive(filename, app) == False and graylog_status.graylog_state() == False and \
